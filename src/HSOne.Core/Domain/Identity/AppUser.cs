f2d3b4f7540962c43a0ec351fd081093577b2ce1
hoangle <hoang.letien@cisbox.com>
2024-11-21 15:38:16 +0700

    feat: add Identity Entity

---
+++ src/HSOne.Core/Domain/Identity/AppUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HSOne.Core.Domain.Identity
+{
+    [Table("AppUsers")]
+    public class AppUser : IdentityUser<Guid>
+    {
+        [Required]
+        [MaxLength(128)]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public required string LastName { get; set; }
+
+        public bool IsActive { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? Dob { get; set; }
+
+        [MaxLength(512)]
+        public string? Avatar { get; set; }
+
+        public DateTime? VipStartDate { get; set; }
+        public DateTime? VipExpireDate { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+        public double Balance { get; set; }
+        public double RoyaltyAmountPerPost { get; set; }
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
