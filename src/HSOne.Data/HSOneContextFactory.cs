a3161eb65e838c38f1d5e59d21fec7f0c314cf01
hoangle <hoang.letien@cisbox.com>
2024-11-21 17:03:51 +0700

    feat: initial Migration database

---
+++ src/HSOne.Data/HSOneContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace HSOne.Data
+{
+    public class HSOneContextFactory : IDesignTimeDbContextFactory<HSOneContext>
+    {
+        public HSOneContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<HSOneContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new HSOneContext(builder.Options);
+        }
+    }
+}
